6d059307b18912d4c6935246d372b7f2012d6966
Sam Webb <samantha.webb2436@gmail.com>
2025-06-18 13:59:52 -0500

    Passed everythinggit add index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    let result = pickUp - 42;
+    return Math.abs(result);
+};
+
+distanceFromHqInBlocks(43);
+
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264;
+};
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264);
+};
+
+  function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    if (distance < 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return ((distance - 400) * .02);
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return "cannot travel that far";
+  };
+};
+
